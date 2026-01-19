191ece73a7dd5d5f79cfc585f21eca4a4f8944e2
Gentsy <160392177+Gentsy13@users.noreply.github.com>
2026-01-19 02:56:44 +0000

    exercise 1

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -9,8 +9,33 @@ import static com.craftinginterpreters.lox.TokenType.*; // belongs to the class
 class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
     Scanner(String source) {
         this.source = source;
     }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+        }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private void scanToken() {
+        char c = advance();
+    }
+
+
+
+
+
 }
